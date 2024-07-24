6d37699be7278d655f43e4d866cd67db17201817
Felipe Vasquez <minelyne@gmail.com>
2024-07-24 17:11:21 -0400

    feat: evaluate math expressions

--- Scripts/Factories/only_positive_floats.gd
+++ Scripts/Factories/only_positive_floats.gd
@@ -1,28 +1,48 @@
 extends LineEdit
 
+const REGEX_PATTERN: String = r"^\d+(\.\d+)?$"
+var regex: RegEx
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	regex = RegEx.new()
+	regex.compile(REGEX_PATTERN)
 	text = "0"
-	text_submitted.connect(_on_text_submitted)
-	focus_exited.connect(_on_text_submitted)
-	pass # Replace with function body.
+	text_submitted.connect(evaluate_math_expression)
+	focus_exited.connect(_on_focus_exited)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	pass
 
-func _on_text_submitted(new_text: String = "") -> void:
-	text = check_input(new_text)
+func _on_focus_exited() -> void:
+	text = check_input(text)
+
+func check_input(input: String) -> String:
+	var match: RegExMatch = regex.search(input)
 	
-
-
-func check_input(new_text:String) -> String:
-	var number: float = 0.0
-	if not new_text.is_empty():
-		number = new_text.to_float()
-	# Verifica si el número es positivo
-	if new_text.is_empty() or number < 0.0:
-		return "0"
+	if match:
+		var value: float = input.to_float()
+		
+		# Validar y manejar casos especiales
+		if value < 0 or input == "-0" or input.strip_edges().is_empty():
+			return "0"
+		
+		# Verificar decimal inválido
+		var parts: PackedStringArray = input.split(".")
+		if parts.size() > 2:
+			return parts[0] # Devolver solo la parte entera
+		
+		return input
 	else:
-		return str(number)
-	
+		# No es un número válido
+		return "0"
+
+func evaluate_math_expression(new_text: String) -> void:
+	var expression: Expression = Expression.new()
+	expression.parse(new_text)
+	var result: Variant = expression.execute()
+	print(result)
+	if result == null:
+		text = "0"
+	text = check_input(str(result))
