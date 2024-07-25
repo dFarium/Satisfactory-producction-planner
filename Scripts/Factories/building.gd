b6731bad52e81b5afe2da4b835b9652d47e67b6a
Felipe Vasquez <minelyne@gmail.com>
2024-07-24 23:12:12 -0400

    input calculator

--- Scripts/Factories/building.gd
+++ Scripts/Factories/building.gd
@@ -1,54 +1,90 @@
 extends GraphNode
 class_name GraphBuilding
 
-@export var inputs:Array[VBoxContainer]
-@export var outputs:Array[VBoxContainer]
-@onready var building_name:Label = $Buildings/BuildingName
-var input_nodes:Array[Array]
-var output_nodes:Array[Array]
+@export var inputs: Array[VBoxContainer]
+@export var outputs: Array[VBoxContainer]
+@onready var building_name: Label = $Buildings/BuildingName
+@onready var building_count_text: LineEdit = $Buildings/LineEdit
+
+var input_nodes: Array[Array]
+var output_nodes: Array[Array]
+
+var input_values: Array[LineEdit] = []
+var output_values: Array[LineEdit] = []
+
+var current_recipe: Recipe
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	set_close_button()	
-	#Label,TextureRect,Spinbox
+	set_close_button()
+	#Label,TextureRect,HBoxContainer(LineEdit,Label)
 	for input in inputs:
 		input_nodes.append(input.get_children())
 		
 	for output in outputs:
 		output_nodes.append(output.get_children())
-
 		
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta:float) -> void:
+func _process(_delta: float) -> void:
+
 	pass
 
-func setup_building(recipe:Recipe) -> void:
+func setup_building(recipe: Recipe) -> void:
+	current_recipe = recipe
 	title = recipe.name
-	building_name.text = get_building_name(recipe.building) +" ("+ str(recipe.manufacturing_cycle) + " sec)"
+	building_name.text = get_building_name(recipe.building) + " (" + str(recipe.manufacturing_cycle) + " sec)"
 	
 	#inputs
 	for i in range(input_nodes.size()):
 		print(recipe.inputs[i].item.name)
 		input_nodes[i][0].text = recipe.inputs[i].item.name
 		input_nodes[i][1].texture = recipe.inputs[i].item.icon
+
+		input_values.append(input_nodes[i][2].get_node("LineEdit"))
 		
 	#outputs
 	for i in range(output_nodes.size()):
 		output_nodes[i][0].text = recipe.outputs[i].item.name
 		output_nodes[i][1].texture = recipe.outputs[i].item.icon
+
+		output_values.append(output_nodes[i][2].get_node("LineEdit"))
+	
+func _on_slot_value_updated(slot: int, value: float) -> void:
+	#value = items per minute
+	slot -= 1
+	print("Slot: " + str(slot) + " Value: " + str(value))
+	var cycles_per_minute: float = 60 / current_recipe.manufacturing_cycle
+	#si slot es un input
+	if slot >= 0 and slot <= 5:
+		var items_per_min_per_building: float = cycles_per_minute * current_recipe.inputs[slot].quantity
+		var building_count: float = value / items_per_min_per_building
+		print("Building count: " + str(building_count))
+
+		building_count_text.text = str(building_count)
+
+		for i in input_values.size():
+			if i != slot:
+				var items_per_min: float = building_count * float(current_recipe.inputs[i].quantity) * cycles_per_minute
+				input_values[i].text = str(items_per_min)
 		
-		
-func update_production_info() -> void:
+		for i in output_values.size():
+			print("Quantity" + str(current_recipe.inputs[i].quantity))
+			var items_per_min: float = building_count * float(current_recipe.outputs[i].quantity) * cycles_per_minute
+			output_values[i].text = str(items_per_min)
+
+	#si slot es un output
+
 	pass
 	
 func set_close_button() -> void:
 	#Close button
-	var hbox:HBoxContainer = get_titlebar_hbox()
-	var btn:Button = Button.new()
+	var hbox: HBoxContainer = get_titlebar_hbox()
+	var btn: Button = Button.new()
 	btn.text = " X "
 	btn.pressed.connect(queue_free)
 	hbox.add_child(btn)
 	
-func get_building_name(building:int) -> String:
+func get_building_name(building: int) -> String:
 	match building:
 		Recipe.Building.Assembler:
 			return "Assembler"
---
+++ Scripts/Factories/building_slot.gd
@@ -0,0 +1,76 @@
+extends LineEdit
+
+const REGEX_PATTERN: String = r"^\d+(\.\d+)?$"
+var regex: RegEx
+var last_valid_value: String = "0"
+
+enum BuildingSlot {
+	#Default
+	Unnasigned,
+	#Inputs
+	Input1,
+	Input2,
+	Input3,
+	Input4,
+	Input5,
+	Input6,
+	#Outputs
+	Output1,
+	Output2,
+	Output3,
+	Output4,
+	Output5,
+	Output6,
+	#Factory
+	Building
+}
+
+signal slot_value_updated(slot: BuildingSlot, value: float)
+
+@export var slot:BuildingSlot
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	regex = RegEx.new()
+	regex.compile(REGEX_PATTERN)
+	text = "0"
+	text_submitted.connect(_on_text_submitted)
+	focus_exited.connect(_on_focus_exited)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	pass
+
+func _on_focus_exited() -> void:
+	evaluate_math_expression(text)
+	slot_value_updated.emit(slot, text.to_float())
+
+func _on_text_submitted(_new_text: String) -> void:
+	print("text submitted")
+	evaluate_math_expression(text)
+	slot_value_updated.emit(slot, text.to_float())
+
+func evaluate_math_expression(new_text: String) -> void:
+	var expression: Expression = Expression.new()
+	expression.parse(new_text)
+	var result: Variant = expression.execute()
+	if result == null:
+		text = last_valid_value
+	text = check_input(str(result))
+
+func check_input(input: String) -> String:
+	var match: RegExMatch = regex.search(input)
+	
+	if match:
+		var value: float = input.to_float()
+		
+		# Validar y manejar casos especiales
+		if value < 0 or input == "-0" or input.strip_edges().is_empty():
+			return last_valid_value
+		
+		#actualizar el valor válido
+		last_valid_value = input
+		return input
+	else:
+		# No es un número válido
+		return last_valid_value
