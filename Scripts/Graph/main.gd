afaecb61fb34f5dc69f39f71d65b8618f7f3335c
Felipe Vasquez <minelyne@gmail.com>
2024-07-31 14:31:43 -0400

    recipes

--- Scripts/Graph/main.gd
+++ Scripts/Graph/main.gd
@@ -2,9 +2,8 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	OS.low_processor_usage_mode = true
 	pass
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	pass
-
-
